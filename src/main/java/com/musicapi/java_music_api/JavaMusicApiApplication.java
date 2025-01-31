4e27ca97cdc91c42e5dec16c9af824007778b4c3
Giorgia <106474194+giorgia834@users.noreply.github.com>
2025-01-31 22:18:36 +0000

    Initial commit

---
+++ src/main/java/com/musicapi/java_music_api/JavaMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.musicapi.java_music_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMusicApiApplication.class, args);
+	}
+
+}
