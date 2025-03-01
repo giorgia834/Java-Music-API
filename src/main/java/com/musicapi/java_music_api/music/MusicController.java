6167008c3129e8c5985660184ce16f1f20185d0f
Giorgia <106474194+giorgia834@users.noreply.github.com>
2025-03-01 14:30:17 +0000

    Update imports

--- src/main/java/com/musicapi/java_music_api/music/MusicController.java
+++ src/main/java/com/musicapi/java_music_api/music/MusicController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
